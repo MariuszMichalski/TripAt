16307b9218e11a68c2a6796dac85d1e92f302751
Mariusz <mario.m4@interia.pl>
2023-01-31 19:01:23 +0100

    Added component Countries

---
+++ src/components/Countries/Countries.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './Countries.css'
+
+export const Countries = () => {
+    return <>
+        <div className="one">
+            <h1>Kraje</h1>
+        </div>
+        <p className="first">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam at est et facilis fuga hic illo magni officiis? Culpa delectus enim magni minima quae sunt! Cum laudantium natus non quo.</p>
+    </>
+}
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import './Main.css'
 import {Header} from "../Header/Header";
+import {Countries} from "../Countries/Countries";
 
 export const Main = () => {
-    return (
+    return <>
         <Header/>
-    )
+        <Countries/>
+    </>
+
 }
