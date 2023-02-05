084243c04b5fa6c5adaddb41d566f5b31705954f
Mariusz <mario.m4@interia.pl>
2023-02-05 18:59:27 +0100

    Little changes in styles and components

--- src/components/Countries/Countries.tsx
+++ src/components/Countries/Countries.tsx
@@ -5,7 +5,7 @@ export const Countries = () => {
     return <>
         <div className="countryWrapper">
             <div className="country">
-                <h1>Kraje</h1>
+                <h1>KRAJE</h1>
             </div>
             <div className="showCountry">
                 <p className="countryP">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam at est et facilis fuga hic illo magni officiis? Culpa delectus enim magni minima quae sunt! Cum laudantium natus non quo.</p>
--- src/components/Shop/Shop.tsx
+++ src/components/Shop/Shop.tsx
@@ -5,7 +5,7 @@ export const Shop = () => {
     return <>
         <div className="shopWrapper">
             <div className="shop">
-                <h1>Sklep</h1>
+                <h1>SKLEP</h1>
             </div>
             <div className="showShop">
                 <p className="shopP">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam at est et facilis fuga hic illo magni officiis? Culpa delectus enim magni minima quae sunt! Cum laudantium natus non quo.</p>
